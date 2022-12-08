1d803dc07c8eccdb067b9fd67047ece074d8aa65
Laszlo Szikszai <laszlo_szikszai@epam.com>
2022-12-08 20:50:00 +0100

    Fib

--- module-1/fibonacci.js
+++ module-1/fibonacci.js
@@ -6,15 +6,6 @@
  * @returns {number} Fibonacci number or 0 if any arguments are not proper
  */
 function fibonacci(n) {
-    /*
-     * Your task is to calculate the nth value of the
-     * Fibonacci sequence.
-     * https://en.wikipedia.org/wiki/Fibonacci_number
-     * Store the value in the nThFibonacci variable.
-     * Also, take into consideration the documentation of the function!
-     */
-    // PLACE YOUR CODE BETWEEN THIS...
-
     if (typeof n !== "number" || n < 0) {
         throw new Error();
     }
